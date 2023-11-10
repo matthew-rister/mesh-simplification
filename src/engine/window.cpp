mjrister/VkTF

refactor: rework window creation
Although functional, the current implementation does not adhere to a few
best practices:
  1. Write short functions
  2. Use member initializer lists
  3. Make interfaces easy to use correctly and hard to use incorrectly

This commit reworks window creation to adhere to these guidelines. 1 and
2 are addressed by extracting GLFW window creation to a separate method.
3 is addressed by adding a singleton interface to GlfwContext which
prevents instantiation multiple copies of it which is a logical_error.

src/engine/window.cpp
@@ -7,23 +7,11 @@
 
 namespace {
 
-using UniqueGlfwWindow = std::unique_ptr<GLFWwindow, void (*)(GLFWwindow*)>;
-
 class GlfwContext {
 public:
-  GlfwContext() {
-    glfwSetErrorCallback([](const int error_code, const char* const description) {
-      std::cerr << std::format("GLFW error {}: {}\n", error_code, description);
-    });
-    if (glfwInit() == GLFW_FALSE) {
-      throw std::runtime_error{"GLFW initialization failed"};
-    }
-#ifdef GLFW_INCLUDE_VULKAN
-    if (glfwVulkanSupported() == GLFW_FALSE) {
-      throw std::runtime_error{"No Vulkan loader or installable client driver"};
-    }
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-#endif
+  static const GlfwContext& GetInstance() {
+    static const GlfwContext kInstance;
+    return kInstance;
   }
 
   GlfwContext(const GlfwContext&) = delete;
@@ -33,23 +21,43 @@ public:
   GlfwContext& operator=(GlfwContext&&) noexcept = delete;
 
   ~GlfwContext() noexcept { glfwTerminate(); }
+
+private:
+  GlfwContext() {
+    glfwSetErrorCallback([](const int error_code, const char* const description) {
+      std::cerr << std::format("GLFW error {}: {}\n", error_code, description);
+    });
+    if (glfwInit() == GLFW_FALSE) {
+      throw std::runtime_error{"GLFW initialization failed"};
+    }
+#ifdef GLFW_INCLUDE_VULKAN
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+#endif
+  }
 };
+
+using UniqueGlfwWindow = std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)>;
+
+UniqueGlfwWindow CreateGlfwWindow(const char* title, const int width, const int height) {
+  [[maybe_unused]] const auto& glfw_context = GlfwContext::GetInstance();
+  auto* glfw_window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+  if (glfw_window == nullptr) throw std::runtime_error{"Window creation failed"};
+  return UniqueGlfwWindow{glfw_window, glfwDestroyWindow};
+}
 
 }  // namespace
 
-gfx::Window::Window(const char* const title, const int width, const int height) {
-  [[maybe_unused]] static const GlfwContext kGlfwContext;
-
-  glfw_window_ = UniqueGlfwWindow{glfwCreateWindow(width, height, title, nullptr, nullptr), glfwDestroyWindow};
-  if (glfw_window_ == nullptr) throw std::runtime_error{"Window creation failed"};
-
+gfx::Window::Window(const char* const title, const int width, const int height)
+    : glfw_window_{CreateGlfwWindow(title, width, height)} {
   glfwSetWindowUserPointer(glfw_window_.get(), this);
-  glfwSetKeyCallback(glfw_window_.get(), [](GLFWwindow* const glfw_window, const int key, const int /*scancode*/,
-                                            const int action, const int /*modifiers*/) {
-    if (const auto* self = static_cast<Window*>(glfwGetWindowUserPointer(glfw_window))) {
-      self->on_key_event_(key, action);
-    }
-  });
+
+  glfwSetKeyCallback(
+      glfw_window_.get(),
+      [](GLFWwindow* glfw_window, const int key, const int /*scancode*/, const int action, const int /*modifiers*/) {
+        if (const auto* self = static_cast<Window*>(glfwGetWindowUserPointer(glfw_window))) {
+          self->on_key_event_(key, action);
+        }
+      });
 }
 
 #ifdef GLFW_INCLUDE_VULKAN

matthew-rister/mesh-simplification

src/engine/window.cpp
@@ -7,23 +7,11 @@
 
 namespace {
 
-using UniqueGlfwWindow = std::unique_ptr<GLFWwindow, void (*)(GLFWwindow*)>;
-
 class GlfwContext {
 public:
-  GlfwContext() {
-    glfwSetErrorCallback([](const int error_code, const char* const description) {
-      std::cerr << std::format("GLFW error {}: {}\n", error_code, description);
-    });
-    if (glfwInit() == GLFW_FALSE) {
-      throw std::runtime_error{"GLFW initialization failed"};
-    }
-#ifdef GLFW_INCLUDE_VULKAN
-    if (glfwVulkanSupported() == GLFW_FALSE) {
-      throw std::runtime_error{"No Vulkan loader or installable client driver"};
-    }
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-#endif
+  static const GlfwContext& Get() {
+    static const GlfwContext kInstance;
+    return kInstance;
   }
 
   GlfwContext(const GlfwContext&) = delete;
@@ -33,23 +21,43 @@ public:
   GlfwContext& operator=(GlfwContext&&) noexcept = delete;
 
   ~GlfwContext() noexcept { glfwTerminate(); }
+
+private:
+  GlfwContext() {
+    glfwSetErrorCallback([](const int error_code, const char* const description) {
+      std::cerr << std::format("GLFW error {}: {}\n", error_code, description);
+    });
+    if (glfwInit() == GLFW_FALSE) {
+      throw std::runtime_error{"GLFW initialization failed"};
+    }
+#ifdef GLFW_INCLUDE_VULKAN
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+#endif
+  }
 };
+
+using UniqueGlfwWindow = std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)>;
+
+UniqueGlfwWindow CreateGlfwWindow(const char* title, const int width, const int height) {
+  [[maybe_unused]] const auto& glfw_context = GlfwContext::Get();
+  auto* glfw_window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+  if (glfw_window == nullptr) throw std::runtime_error{"Window creation failed"};
+  return UniqueGlfwWindow{glfw_window, glfwDestroyWindow};
+}
 
 }  // namespace
 
-gfx::Window::Window(const char* const title, const int width, const int height) {
-  [[maybe_unused]] static const GlfwContext kGlfwContext;
-
-  glfw_window_ = UniqueGlfwWindow{glfwCreateWindow(width, height, title, nullptr, nullptr), glfwDestroyWindow};
-  if (glfw_window_ == nullptr) throw std::runtime_error{"Window creation failed"};
-
+gfx::Window::Window(const char* const title, const int width, const int height)
+    : glfw_window_{CreateGlfwWindow(title, width, height)} {
   glfwSetWindowUserPointer(glfw_window_.get(), this);
-  glfwSetKeyCallback(glfw_window_.get(), [](GLFWwindow* const glfw_window, const int key, const int /*scancode*/,
-                                            const int action, const int /*modifiers*/) {
-    if (const auto* self = static_cast<Window*>(glfwGetWindowUserPointer(glfw_window))) {
-      self->on_key_event_(key, action);
-    }
-  });
+
+  glfwSetKeyCallback(
+      glfw_window_.get(),
+      [](GLFWwindow* glfw_window, const int key, const int /*scancode*/, const int action, const int /*modifiers*/) {
+        if (const auto* self = static_cast<Window*>(glfwGetWindowUserPointer(glfw_window))) {
+          self->on_key_event_(key, action);
+        }
+      });
 }
 
 #ifdef GLFW_INCLUDE_VULKAN